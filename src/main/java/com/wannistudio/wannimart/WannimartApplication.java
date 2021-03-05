9c16075da737abb4613dffa4a817270fdc056019
wanni0928 <chlgudwnd123@gmail.com>
2021-03-05 23:36:35 +0900

    init project

---
+++ src/main/java/com/wannistudio/wannimart/WannimartApplication.java
@@ -0,0 +1,13 @@
+package com.wannistudio.wannimart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WannimartApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(WannimartApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/wannistudio/wannimart/WannimartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wannistudio.wannimart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WannimartApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
