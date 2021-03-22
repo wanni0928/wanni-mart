54605be8581e0156ce67e8112499b15398802f1c
wanni0928 <chlgudwnd123@gmail.com>
2021-03-22 11:42:08 +0900

    feat: add notice domain

---
+++ src/main/java/com/wannistudio/wannimart/domain/notice/Notice.java
@@ -0,0 +1,28 @@
+package com.wannistudio.wannimart.domain.notice;
+
+import com.wannistudio.wannimart.domain.member.Member;
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class Notice {
+  @Id
+  @GeneratedValue
+  @Column(name = "notice_id")
+  private Long id;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "member_id")
+  private Member member;
+
+  private String title;
+  private String author;
+  private LocalDateTime dateCreated;
+  private int hit;
+}
