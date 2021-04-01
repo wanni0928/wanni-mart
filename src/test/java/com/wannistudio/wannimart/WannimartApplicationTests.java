954cc7fa56e94a58b47b348fe4528152f8494d6a
wanni0928 <chlgudwnd123@gmail.com>
2021-04-02 01:55:19 +0900

    gcp에서 centos로 yum이 갑자기 안먹히는 버그 발생.. 우분투로 갈아타서 다시 작업.

--- src/test/java/com/wannistudio/wannimart/WannimartApplicationTests.java
+++ src/test/java/com/wannistudio/wannimart/WannimartApplicationTests.java
@@ -1,8 +1,7 @@
 package com.wannistudio.wannimart;
 
-import com.wannistudio.wannimart.config.JasyptConfig;
+
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
