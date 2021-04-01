64635ba0e78ba142e5955ce3eaeff621837ae777
wanni0928 <chlgudwnd123@gmail.com>
2021-04-02 01:33:46 +0900

    .

--- src/main/java/com/wannistudio/wannimart/config/RedisConfig.java
+++ src/main/java/com/wannistudio/wannimart/config/RedisConfig.java
@@ -2,10 +2,12 @@ package com.wannistudio.wannimart.config;
 
 import com.wannistudio.wannimart.domain.connect.ItemCategoryQueryDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.Page;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
@@ -18,9 +20,23 @@ import java.util.List;
 @RequiredArgsConstructor
 public class RedisConfig {
 
+  @Value("${spring.redis.host}")
+  private String redisHost;
+
+  @Value("${spring.redis.port}")
+  private int redisPort;
+
+  @Value("${spring.redis.password}")
+  private String redisPassword;
+
   @Bean
   public RedisConnectionFactory redisConnectionFactory() {
-    LettuceConnectionFactory lettuceConnectionFactory = new LettuceConnectionFactory();
+    RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+    redisStandaloneConfiguration.setHostName(redisHost);
+    redisStandaloneConfiguration.setPort(redisPort);
+    redisStandaloneConfiguration.setPassword(redisPassword);
+    LettuceConnectionFactory lettuceConnectionFactory = new LettuceConnectionFactory(
+            redisStandaloneConfiguration);
     return lettuceConnectionFactory;
   }
 
